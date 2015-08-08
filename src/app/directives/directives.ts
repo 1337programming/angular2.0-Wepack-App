e3af5c07396291a0e8150ad621ac75af36faa666
opiepj <opiepj@plu.edu>
2015-08-08 15:56:28 -0700

    (#6) Auto focus public directive

--- src/app/directives/directives.ts
+++ src/app/directives/directives.ts
@@ -1,6 +1,9 @@
 /// <reference path="../../typings/_custom.d.ts" />
 /*
  * Angular 2
+ * @coreDirectives: collection of Angular core directives
+ * @formDirectives: list of all the form directives
+ * @routerDirectives: list of all the router directives
  */
 import {coreDirectives, formDirectives} from 'angular2/angular2';
 import {routerDirectives} from 'angular2/router';
