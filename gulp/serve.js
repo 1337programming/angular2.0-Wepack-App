e56d72a28351bc15421bb78990293bae9691f01d
Tom Manion <thomaswmanion@gmail.com>
2015-08-10 22:35:40 -0700

    Fixed the livereload when at a non-root URL problem.

--- gulp/serve.js
+++ gulp/serve.js
@@ -2,6 +2,11 @@ var gulp = require('gulp');
 var webpack = require('webpack-stream');
 var webpackConfig = require('../webpack.config.js');
 var browserSync = require('browser-sync');
+var browserSyncSpa = require('browser-sync-spa');
+
+browserSync.use(browserSyncSpa({
+  selector: '[ng-app]'// Only needed for angular apps
+}));
 
 gulp.task('serve', ['webpack', 'watch'], function() {
 	var server = {
